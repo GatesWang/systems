1776d0a92bcd0eaa24b12f790fdfb600a4690cec
Gates Wang <gateswang00@gmail.com>
2020-10-25 13:44:29 -0400

    working memgrind

--- asst/asst1/malloc.c
+++ asst/asst1/malloc.c
@@ -1,15 +1,90 @@
-
 #include <stdio.h>
 
 #define malloc(x) mymalloc(x,__FILE__,__LINE__)
 #define free(x) myfree(x,__FILE__,__LINE__)
 
+typedef struct Metadata{
+	void *ptr;
+	int start;
+	int end;
+} Metadata;
+
+Metadata createData(void *ptr, int start, int end){
+	Metadata data;
+	data.ptr = ptr;
+	data.start = start;
+	data.end = end;
+	return data;
+}
+
 static char myblock[4096];
-void *pointers[4096];
-int insertion = 0;
+static Metadata data[4096];
+int memory_insertion = 0;
+int data_insertion = 0;
+
+int is_in_range(void *ptr);
+
+void remove_memory_block(int start, int end);
+void remove_data_at_index(int index);
+void insert_data(Metadata data);
 
 void myfree(void *pointer, char *file, int line){
+	int valid = is_in_range(pointer);
+	if(valid == 0){
+		printf("not in range\n");
+		return;
+	}
+	for(int i=0; i<4096; i++){
+		if(data[i].ptr == pointer){
+			Metadata metadata = data[i];
+			remove_memory_block(metadata.start, metadata.end);
+			remove_data_at_index(i);
+			return;
+		}
+	}
+	printf("not allocated by malloc\n");
+}
 
+void* mymalloc(size_t size, char *file, int line){
+	if(memory_insertion + size > 4096){
+		printf("not enough memory\n");
+		return NULL;
+	}
+	else{
+		int start = memory_insertion;
+		void *pointer = &myblock[memory_insertion];
+		for(int i=0; i<size; i++){
+			myblock[memory_insertion++] = 'a';
+		}
+		int end = memory_insertion-1;
+		Metadata metadata = createData(pointer, start, end);
+		data[data_insertion++] = metadata;
+		return pointer;
+	}
+}
+
+int memory_used(){
+	return memory_insertion;
+}
+
+int pointers_stored(){
+	return data_insertion;
+}
+
+/*
+	input: a pointer
+	this determines whether the pointer given is a valid address
+	in myblock
+*/
+int is_in_range(void *ptr){
+	void *start = &myblock[0];
+	void *end = &myblock[4096];
+	if(ptr>=start && ptr<=end){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 /*
@@ -27,28 +102,22 @@ void print_memory(int start, int end){
 	this attempts to fill in the block by shifting everything
 	to the right of the block over
 */
-void shift(int start, int end){
+void remove_memory_block(int start, int end){
 	int write = start;
-	for(int read=end+1; read<20; read++){
-		myblock[write] = myblock[read];
-		//print_memory(start,end);
-		write++;
+	for(int read=end+1; read<4096; read++){
+		myblock[write++] = myblock[read];
 	}
+	int length = end - start + 1;
+	memory_insertion -= length;
 }
 
-void * mymalloc(size_t size, char *file, int line){
-	char *start = &myblock[0];
-	if(insertion + size > 4096){
-		printf("error\n");
+/*
+	This removes the metadata stored at index, while shifting everything to fill in the gap created.
+*/
+void remove_data_at_index(int index){
+	int write = index;
+	for(int read=index+1; read<4096; read++){
+		data[write++] = data[read];
 	}
-	else{
-		for(int i=0; i<size; i++){
-			myblock[insertion] = 'a';
-			insertion++;
-		}
-	}
-	//free();
-	printf("\n");
-	return start;
+	data_insertion--;
 }
-
--- asst/asst1/memgrind.c
+++ asst/asst1/memgrind.c
@@ -1,11 +1,224 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 #include "malloc.c"
 
-int main(int argc, char*argv[]){
-	malloc(5000);
-	malloc(3);
-	print_memory(0,20);
-	shift(0,2);
-	print_memory(0,20);
+suseconds_t workload_a();
+suseconds_t workload_b();
+suseconds_t workload_c();
+suseconds_t workload_d();
 
+void allocate_or_free(void* pointers[], int length, int* pointer_index, int free, int create, int size);
+void free_pointers(void* pointers[], int length, int* pointer_index);
+void basic_tests();
+void print_info(char* string);
+
+int main(int argc, char*argv[]){
+	double time_a = 0;
+	double time_b = 0;
+	double time_c = 0;
+	double time_d = 0;
+	/*
+	int length = 120;
+	void* pointers[length];
+	int pointer_index = -1;
+	int free = 2;
+	int create = 5;
+	int size = 100;
+	*/
+	for(int workload=0; workload<5; workload++){
+	/*
+		print_info("before: ");
+		allocate_or_free(pointers, length, &pointer_index, free, create, size);
+		allocate_or_free(pointers, length, &pointer_index, free, create, size);
+		print_info("after: ");
+		free_pointers(pointers, length, &pointer_index);
+		print_info("after free: ");
+		printf("\n");
+	*/
+		//time_a += workload_a();
+		//time_b += workload_b();
+		//time_c += workload_c();
+		time_d += workload_d();
+	}
+
+	time_a = time_a/50;
+	time_b = time_b/50;
+	time_c = time_c/50;
+	time_d = time_d/50;
+	char output[] ="workload a: %f\nworkload b: %f\nworkload c: %f\nworkload d: %f\n";
+	printf(output, time_a,time_b,time_c,time_d);
+}
+
+
+/*
+	returns how long the workload took to execute
+*/
+suseconds_t workload_a(){
+	struct timeval difference;
+	struct timeval start_time;
+	struct timeval end_time;
+	gettimeofday(&start_time, NULL);
+
+	for(int i=0; i<120; i++){
+		char *ptr = malloc(1);
+		free(ptr);
+	}
+
+	gettimeofday(&end_time, NULL);
+	timersub(&end_time, &start_time, &difference);
+	suseconds_t diff  = difference.tv_usec;
+	return diff;
+}
+
+
+/*
+	returns how long the workload took to execute
+*/
+suseconds_t workload_b(){
+	struct timeval difference;
+	struct timeval start_time;
+	struct timeval end_time;
+	gettimeofday(&start_time, NULL);
+
+	void* pointers[120];
+	for(int i=0; i<120; i++){
+		pointers[i] = malloc(1);
+	}
+	for(int i=0; i<120; i++){
+		free(pointers[i]);
+	}
+
+	gettimeofday(&end_time, NULL);
+	timersub(&end_time, &start_time, &difference);
+	suseconds_t diff  = difference.tv_usec;
+	return diff;
+}
+
+/*
+	returns how long the workload took to execute
+*/
+suseconds_t workload_c(){
+	struct timeval difference;
+	struct timeval start_time;
+	struct timeval end_time;
+	gettimeofday(&start_time, NULL);
+
+	int length = 120;
+	void* pointers[length];
+	int pointer_index = -1;
+	int free = 1;
+	int create = 1;
+	int size = 1;
+
+	for(int i=0; i<240; i++){
+		allocate_or_free(pointers, length, &pointer_index, free, create, size);
+	}
+
+	gettimeofday(&end_time, NULL);
+	timersub(&end_time, &start_time, &difference);
+	suseconds_t diff  = difference.tv_usec;
+	return diff;
+}
+
+
+/*
+	returns how long the workload took to execute
+*/
+suseconds_t workload_d(){
+	struct timeval difference;
+	struct timeval start_time;
+	struct timeval end_time;
+	gettimeofday(&start_time, NULL);
+
+	int length = 120;
+	void* pointers[length];
+	int pointer_index = -1;
+	int free = 5;
+	int create = 2;
+	int size = 100;
+
+	int memory = memory_used();
+	int ptrs = pointers_stored();
+	printf("before--- memory: %d, pointers: %d\n", memory, ptrs);
+
+	for(int i=0; i<240; i++){
+		allocate_or_free(pointers, length, &pointer_index, free, create, size);
+	}
+	//free_pointers(pointers, length, pointer_index);
+
+	memory = memory_used();
+	ptrs = pointers_stored();
+	printf("after--- memory: %d, pointers: %d\n", memory, ptrs);
+
+	gettimeofday(&end_time, NULL);
+	timersub(&end_time, &start_time, &difference);
+	suseconds_t diff  = difference.tv_usec;
+	return diff;
+}
+
+/*
+	allocates pointers
+*/
+void allocate_or_free(void* pointers[], int length, int* pointer_index, int free, int create, int size){
+	int num = rand() % (free+create);
+	if(num < free){//remove
+		if((*pointer_index) >= 0){
+			free(pointers[(*pointer_index)--]);
+		}
+	}
+	else{//add
+		if((*pointer_index) < length){
+			pointers[++(*pointer_index)] = malloc(size);
+		}
+	}
+}
+
+/*
+	free all of the pointers stored in the array
+*/
+void free_pointers(void* pointers[], int length, int* pointer_index){
+	for(; (*pointer_index)>=0; (*pointer_index)--){
+		free(pointers[*(pointer_index)]);
+	}
+}
+
+void basic_tests(){
+	printf("1: should be not enough memory\n");
+	malloc(5000);
+
+	printf("2: should be not enough memory\n");
+	void *pointer = malloc(4096);
+	malloc(1);
+	free(pointer);
+
+	printf("3: should be not allocated by malloc\n");
+	char *p = (char*) malloc(200);
+	free(p+10);
+
+	printf("4: should be not allocated by malloc\n");
+	int *y;
+	free(y);
+
+	printf("5: should be not allocated by malloc\n");
+	int x;
+	free((int*)x);
+
+	printf("6: should be not allocated by malloc\n");
+	p =(char *) malloc(100);
+	free(p);
+	free(p);
+
+	printf("7: should be no error\n");
+	p =(char *) malloc(100);
+	free(p);
+	p =(char *) malloc(100);
+	free(p);
+}
+
+void print_info(char* string){
+	int memory = memory_used();
+	int pointers = pointers_stored();
+	printf("%s memory: %d, pointers: %d\n",string, memory, pointers);
 }
